259e1ec8d8a19e4b693403471c0911e0baa052fd
hickshj <hickshj@rose-hulman.edu>
2018-09-24 11:01:13 -0400

    Toccata and Fugue in D minor

--- src/m4_more_iterating.py
+++ src/m4_more_iterating.py
@@ -21,8 +21,8 @@ def main():
     """ Calls the   TEST   functions in this module. """
     # run_test_sum_sequence()
     # run_test_count_items_bigger_than()
-    run_test_count_positive_sines()
-    # run_test_sum_first_n()
+    # run_test_count_positive_sines()
+    run_test_sum_first_n()
 
 
 def run_test_sum_sequence():
@@ -415,7 +415,7 @@ def count_items_bigger_than(numbers, threshold):
 def run_test_count_positive_sines():
     """ Tests the   count_positive_sines   function. """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement this TEST function.
+    # Done: 6. Implement this TEST function.
     #   It TESTS the  count_positive_sines  function defined below.
     #   Include at least ** 1 ** ADDITIONAL test beyond what we supplied.
     #
@@ -512,6 +512,13 @@ def run_test_count_positive_sines():
 
     # TO DO 6 (continued):  Add your 1 ADDITIONAL test here:
 
+    # Test 11
+    expected = 3
+    actual = count_positive_sines([1, 17, 12, 32, 2])
+    print()
+    print('Test 11 expected:', expected)
+    print('        actual:  ', actual)
+
 
 def count_positive_sines(numbers):
     """
@@ -537,9 +544,15 @@ def count_positive_sines(numbers):
       :type sequence: list or tuple (of numbers)
     """
     # ------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # Done: 7. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
+    total = 0
+    for k in range(len(numbers)):
+        a = math.sin(numbers[k])
+        if a > 0:
+            total = total + 1
+    return total
 
 
 def run_test_sum_first_n():
