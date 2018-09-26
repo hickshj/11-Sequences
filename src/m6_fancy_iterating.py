68e1e37af791612acca0af20b4451f033c950d5e
hickshj <hickshj@rose-hulman.edu>
2018-09-26 10:07:01 -0400

    Toccata and Fugue in D minor

--- src/m6_fancy_iterating.py
+++ src/m6_fancy_iterating.py
@@ -52,8 +52,8 @@ def main():
     # run_test_print_items_at_odd_indices(sequence1, sequence2, sequence3, sequence4)
     # run_test_print_items_in_second_half(sequence1, sequence2, sequence3, sequence4)
     # run_test_print_items_that_are_bigger_than_5()  # Uses different sequences
-    run_test_print_items_that_are_strings(sequence1, sequence2, sequence3, sequence4)
-    # run_test_print_items_that_are_odd_integers(sequence1, sequence2, sequence3, sequence4)
+    # run_test_print_items_that_are_strings(sequence1, sequence2, sequence3, sequence4)
+    run_test_print_items_that_are_odd_integers(sequence1, sequence2, sequence3, sequence4)
 
 
 def run_test_print_all_items_forwards(sequence1, sequence2, sequence3, sequence4):
@@ -521,11 +521,17 @@ def print_items_that_are_odd_integers(sequence):
       33 is at index 5
     """
     # ------------------------------------------------------------------
-    # TODO: 9. Implement and test this function.
+    # Done: 9. Implement and test this function.
     #
     # IMPORTANT:  The  type  function returns  int  if its argument
     #   is an integer.  Note that   int   has NO quotes surrounding it.
     # ------------------------------------------------------------------
+    for k in range(len(sequence)):
+        a = sequence[k]
+        if type(a) is int:
+            if a % 2 == 1:
+                print(a)
+
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
